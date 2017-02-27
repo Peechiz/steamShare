4fb2408d8af2c3db842224413553f42e4cb428a4
Chris Impicciche <cimpicci@gmail.com>
2017-02-26 20:38:20 -0600

    fixes prop references, removes extraneous key property

--- src/Game.js
+++ src/Game.js
@@ -1,21 +1,16 @@
 import React, { Component } from 'react';
-import tf2 from './assets/images/tf2.jpg'
 
 class Game extends Component {
-
-  constructor(props){
-    super(props);
-  }
-
   render() {
+    console.log(this.props);
     return (
-      <div className="game" key={id}>
-        <img src={this.props.img} alt={this.props.title}/>
+      <div className="game">
+        <img src={this.props.detail.img} alt={this.props.detail.title}/>
         <div className="gameDetail">
           <ul>
-            <li>Title: {this.props.title}</li>
-            <li>Hours: {this.props.hours}</li>
-            <li><a href={this.props.link}>Store Page</a></li>
+            <li>Title: {this.props.detail.title}</li>
+            <li>Hours: {this.props.detail.hours}</li>
+            <li><a href={this.props.detail.link}>Store Page</a></li>
           </ul>
         </div>
       </div>
