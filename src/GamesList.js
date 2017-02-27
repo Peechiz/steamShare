b0209fb136cf4dd25fc4c5b175248d63e265ba7c
Chris Impicciche <cimpicci@gmail.com>
2017-02-26 20:40:38 -0600

    currectly passes props to each <Game>, adds input for filtering later

--- src/GamesList.js
+++ src/GamesList.js
@@ -2,20 +2,22 @@ import React, { Component } from 'react';
 import Game from './Game';
 
 class GamesList extends Component {
-  const game = this.state.games.map(game =>
-    <Game detail={game.detail}>
-  )
   render() {
+    console.log(this.props.games);
+    const games = this.props.games.map((game, id) =>
+      <Game detail={game} key={id}/>
+      )
     return (
       <div className="col-sm-5">
         <div className="well">
           <h2>My Games</h2>
           <p>Click to share!</p>
           <hr />
-          <Game/>
+          <label>Filter: </label>
+          <input type="text" className="form-control"/>
+          <div>{games}</div>
         </div>
-      </div>
-    )
+      </div>)
   }
 }
 
