c09ad8931e9258ad9f54f93611eb121f747b1de5
Christopher Impicciche <cimpicci@gmail.com>
2017-03-06 14:56:47 -0600

    adds functions to handle select/deselect of game objects

--- src/App.js
+++ src/App.js
@@ -3,25 +3,76 @@ import Header from './Header';
 // import Footer from './Footer';
 import UserSearch from './UserSearch';
 import GamesList from './GamesList';
-import ShareBox from './ShareBox';
+import ShareList from './ShareList';
 import tf2 from './assets/images/tf2.jpg'
-
-
 
 class App extends Component {
 
   constructor(props){
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.state = {games: []}
+    this.handleSelect = this.handleSelect.bind(this);
+    this.handleDeselect = this.handleDeselect.bind(this);
+
+    this.state = {games: [
+      {
+        "title": "Team Fortress 2",
+        "hours": 411,
+        "img": tf2,
+        "url": "http://zombo.com"
+      },
+      {
+        "title": "Rocket League",
+        "hours": 297,
+        "img": tf2,
+        "url": "http://zombo.com"
+      },
+      {
+        "title": "Terraria",
+        "hours": 82,
+        "img": tf2,
+        "url": "http://zombo.com"
+      }]
+    }
   }
 
   handleSubmit(fetched) {
-    this.setState({games: fetched})
+    var games = fetched.map(x => {
+      x.selected = 'false';
+      return x;
+    });
+    this.setState({games: games});
+  }
+
+  handleSelect(item) {
+    let games = this.state.games
+    games.map(game => {
+      if (game.title === item.title){
+        game.selected = true;
+      }
+      return game;
+    })
+    this.setState({games: games})
+  }
+
+  handleDeselect(item) {
+    let games = this.state.games
+    games.map(game => {
+      if (game.title === item.title){
+        game.selected = false;
+      }
+      return game;
+    })
+    this.setState({games: games})
   }
 
   render() {
-    const games = this.state.games;
+    const games = this.state.games.filter(game => {
+      return !game.selected;
+    })
+    const selected = this.state.games.filter(game => {
+      return game.selected;
+    });
     return (
       <div>
         <Header />
@@ -33,8 +84,8 @@ class App extends Component {
           </div>
 
           <UserSearch onSubmit={this.handleSubmit}/>
-          <GamesList games={games}/>
-          <ShareBox/>
+          <GamesList games={games} onSelect={this.handleSelect}/>
+          <ShareList items={selected} onDeselect={this.handleDeselect}/>
 
       </div>
     );
